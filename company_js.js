87d6c094ca5fb7c483f325207631b00baf7d5c25
K-hiroto-act <67185375+K-hiroto-act@users.noreply.github.com>
2020-08-27 21:19:36 +0900

    Add files via upload

---
+++ company_js.js
@@ -0,0 +1,67 @@
+var swiper = new Swiper ('.swiper-container', {
+    loop: true,
+    paginationClickable: true,
+    slidesPerView: 1,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+  });
+  
+  $(function(){
+var topBtn = $('.page-top');
+topBtn.hide();
+$(window).scroll(function () {
+$(this).scrollTop() > 100 ? topBtn.fadeIn() : topBtn.fadeOut();
+});
+topBtn.click(function () {
+$('body,html').animate({ scrollTop: 0 }, 500);
+return false;
+});
+});
+
+gsap.to('.Concept_wrap', {
+    duration: 1, // 右側に2秒かけて移動するモーションを指定する
+    y:'0px',
+    opacity:1,
+    stagger: {
+        each:0.5,
+    }
+  });
+
+  gsap.to('.picture img', {
+    duration: 2, // 右側に2秒かけて移動するモーションを指定する
+    y:'0px',
+    opacity:1,
+    stagger: {
+        each:0.5,
+    }
+  });
+
+  gsap.to('.mission', {
+    duration: 3, // 右側に2秒かけて移動するモーションを指定する
+    y:'0px',
+    opacity:1,
+    stagger: {
+        each:0.5,
+    }
+  });
+
+  gsap.to('.intention', {
+    duration: 4, // 右側に2秒かけて移動するモーションを指定する
+    y:'0px',
+    opacity:1,
+    stagger: {
+        each:0.5,
+    }
+  });
+
+  gsap.to('.Corporate_information', {
+    duration: 2, // 右側に2秒かけて移動するモーションを指定する
+    y:'0px',
+    opacity:1,
+    stagger: {
+        each:0.5,
+    }
+  });
+
+
+
